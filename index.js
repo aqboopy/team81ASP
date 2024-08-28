346f675c2d1ace63fb0bdb007ca0689e5e5de55e
rachC0 <78367994+rachC0@users.noreply.github.com>
2024-08-29 01:39:41 +0800

    Add files via upload
    Added in express-session, body-parser and express-validator

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
 const sqlite3 = require("sqlite3").verbose();
+var bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +11,17 @@ const PORT = process.env.PORT || 3000;
 // Use express-ejs-layouts
 app.use(expressLayouts);
 app.set("layout", "layout"); // Default layout file
+
+//Use body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Use express-session
+app.use(session({
+    secret: 'team81asp',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }));
 
 // Set EJS as the template engine
 app.set("view engine", "ejs");
@@ -19,22 +32,24 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 // Database setup
-const db = new sqlite3.Database(path.join(__dirname, "database", "app.db"));
+global.db = new sqlite3.Database(path.join(__dirname, "database", "app.db"));
 
 // Sample table creation
-db.serialize(() => {
-	db.run(
-		"CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT)"
+global.db.serialize(() => {
+	global.db.run(
+		"CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)"
 	);
 });
 
 // Import routes
 const indexRouter = require("./routes/index");
 const loginRouter = require("./routes/login");
+const registerRouter = require("./routes/register");
 
 // Use routes
 app.use("/", indexRouter);
 app.use("/login", loginRouter);
+app.use("/register", registerRouter);
 
 // Start the server
 app.listen(PORT, () => {
