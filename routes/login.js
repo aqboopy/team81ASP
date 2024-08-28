4e2cc53410b3d07065683bb15ceddc246dfd7818
rachC0 <78367994+rachC0@users.noreply.github.com>
2024-08-29 01:37:50 +0800

    Add files via upload
    after logging in/registering, user will be redirected back to home page

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,58 @@
 const express = require("express");
 const router = express.Router();
 
+const {check, validationResult} = require('express-validator');
+
+//login validation rules
+const loginValidationRules = ()=>{
+    return[
+        check("email","Please input your email!")
+            .isEmail(),
+        check("password","Please input your password!")
+            .isLength({min:1,max:50})
+    ];
+};
+//render login page
 router.get("/", (req, res) => {
 	res.render("Login", { title: "Login/Sign Up" });
 });
+//handle login submission
+router.post("/",loginValidationRules(),(req,res)=>{
+    //get values from form
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //validation
+    var failAlert = null;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+
+        const alert = errors.array();
+        res.render("login",{alert,title: "Login/Sign Up"});
+    }
+    else{
+        let sqlQuery = "SELECT * FROM users where email=?";
+        global.db.get(sqlQuery,[email],(err,result)=>{
+            if(err){
+                console.log(err);
+            }
+            //if cannot find user
+            if(result == null){
+                failAlert = {message: "Email not found!"};
+                return res.render("login",{failAlert,title: "Login/Sign Up"});
+            }
+            //else check password matches
+            else{
+                if(password != result.password){
+                    failAlert = {message: "Incorrect password!"};
+                    return res.render("login",{failAlert,title: "Login/Sign Up"});
+                }
+            }
+			req.session.userdata = result;
+            res.redirect("/");
+        });
+    }
+});
+
 
 module.exports = router;
---
+++ routes/register.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const {check, validationResult} = require('express-validator');
+
+const registerValidationRules = ()=>{
+    return[
+        check("username","Please input your name!")
+            .isLength({min:1,max:50}),
+        check("email","Please input your email!")
+            .isEmail(),
+        check("password","Please input your password!")
+            .isLength({min:1,max:50})
+    ];
+};
+//render register page
+router.get("/", (req, res) => {
+	res.render("register",{ title: "Register" });
+});
+//handle register submission
+router.post("/",registerValidationRules(),(req,res)=>{
+    //get values from form
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //validation
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+
+        const alert = errors.array();
+        res.render("register",{alert,title: "Register"});
+    }
+    else{
+        let insertQuery = "INSERT INTO users (name,email,password) VALUES (?,?,?)";
+        global.db.run(insertQuery,[username,email,password],(err,result)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                let sqlQuery = "SELECT * FROM users WHERE email=?"
+                global.db.get(sqlQuery,[email],(err2,result2)=>{
+                    if(err2){
+                        console.log(err2);
+                    }
+                    req.session.userData = result2;
+                    res.redirect("/");
+                });
+            }         
+            
+        });
+    }
+});
+
+module.exports = router;
