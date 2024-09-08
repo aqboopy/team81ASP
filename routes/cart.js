475f542d2cb8f91bc5b4b49adc0fe4b046b654d2
Henry <nyanlinaung3722@gmail.com>
2024-09-08 21:13:51 +0800

    tweaked home screen carousel

--- routes/cart.js
+++ routes/cart.js
@@ -4,141 +4,158 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.post('/add/:productId', authMiddleware, (req, res) => {
-    const productId = req.params.productId;
-    const userId = req.session.userdata.id;
-    const quantity = req.body.quantity || 1; // Default to 1 if no quantity provided
+router.post("/add/:productId", authMiddleware, (req, res) => {
+	const productId = req.params.productId;
+	const userId = req.session.userdata.id;
+	const quantity = req.body.quantity || 1; // Default to 1 if no quantity provided
 
-    if (!userId) {
-        return res.status(401).json({ error: "Sign in before adding to cart." });
-    }
+	if (!userId) {
+		return res
+			.status(401)
+			.json({ error: "Sign in before adding to cart." });
+	}
 
-    const checkCartQuery = "SELECT * FROM cart WHERE user_id = ? AND product_id = ?";
-    
-    global.db.get(checkCartQuery, [userId, productId], (err, row) => {
-        if (err) {
-            console.error(err.message);
-            return res.status(500).json({ error: "Internal server error." });
-        }
+	const checkCartQuery =
+		"SELECT * FROM cart WHERE user_id = ? AND product_id = ?";
 
-        if (row) {
-            // Product already in cart
-            const updateCartQuery = "UPDATE cart SET quantity = quantity + ? WHERE user_id = ? AND product_id = ?";
-            
-            global.db.run(updateCartQuery, [quantity, userId, productId], function(err) {
-                if (err) {
-                    console.error(err.message);
-                    return res.status(500).json({ error: "Internal server error." });
-                }
-                res.status(200).json({ message: "Product quantity updated in cart!" });
-            });
-        } else {
-            // Product not in cart
-            const insertCartQuery = "INSERT INTO cart (user_id, product_id, quantity) VALUES (?, ?, ?)";
-            
-            global.db.run(insertCartQuery, [userId, productId, quantity], function(err) {
-                if (err) {
-                    console.error(err.message);
-                    return res.status(500).json({ error: "Internal server error." });
-                }
-                res.status(200).json({ message: "Product added to cart successfully!" });
-            });
-        }
-    });
+	global.db.get(checkCartQuery, [userId, productId], (err, row) => {
+		if (err) {
+			console.error(err.message);
+			return res.status(500).json({ error: "Internal server error." });
+		}
+
+		if (row) {
+			// Product already in cart
+			const updateCartQuery =
+				"UPDATE cart SET quantity = quantity + ? WHERE user_id = ? AND product_id = ?";
+
+			global.db.run(
+				updateCartQuery,
+				[quantity, userId, productId],
+				function (err) {
+					if (err) {
+						console.error(err.message);
+						return res
+							.status(500)
+							.json({ error: "Internal server error." });
+					}
+					res.status(200).json({
+						message: "Product quantity updated in cart!",
+					});
+				}
+			);
+		} else {
+			// Product not in cart
+			const insertCartQuery =
+				"INSERT INTO cart (user_id, product_id, quantity) VALUES (?, ?, ?)";
+
+			global.db.run(
+				insertCartQuery,
+				[userId, productId, quantity],
+				function (err) {
+					if (err) {
+						console.error(err.message);
+						return res
+							.status(500)
+							.json({ error: "Internal server error." });
+					}
+					res.status(200).json({
+						message: "Product added to cart successfully!",
+					});
+				}
+			);
+		}
+	});
 });
 
-
 // Route to handle removing a product from the cart
-router.post('/remove/:id', authMiddleware, (req, res) => {
-    const userId = req.session.userdata.id;
-    const productId = req.params.id;
+router.post("/remove/:id", authMiddleware, (req, res) => {
+	const userId = req.session.userdata.id;
+	const productId = req.params.id;
 
-    const removeQuery = `
+	const removeQuery = `
         DELETE FROM cart
         WHERE user_id = ? AND product_id = ?
     `;
 
-    global.db.run(removeQuery, [userId, productId], (err) => {
-        if (err) {
-            console.error(err.message);
-            return res.status(500).send("Internal server error.");
-        }
+	global.db.run(removeQuery, [userId, productId], (err) => {
+		if (err) {
+			console.error(err.message);
+			return res.status(500).send("Internal server error.");
+		}
 
-        // Redirect to cart page with a success message
-        res.redirect('/cart?update=remove-success');
-    });
+		// Redirect to cart page with a success message
+		res.redirect("/cart?update=remove-success");
+	});
 });
 
-
-
 // Route to display the user's cart
-router.get('/', authMiddleware, (req, res) => {
-    const userId = req.session.userdata.id;
+router.get("/", authMiddleware, (req, res) => {
+	const userId = req.session.userdata.id;
 
-    const cartQuery = `
+	const cartQuery = `
         SELECT products.*, cart.quantity
         FROM cart
         JOIN products ON cart.product_id = products.id
         WHERE cart.user_id = ?
     `;
 
-    global.db.all(cartQuery, [userId], (err, cartItems) => {
-        if (err) {
-            console.error(err.message);
-            return res.status(500).send("Internal server error.");
-        }
+	global.db.all(cartQuery, [userId], (err, cartItems) => {
+		if (err) {
+			console.error(err.message);
+			return res.status(500).send("Internal server error.");
+		}
 
-        // Convert BLOB data to Base64 for each cart item
-        cartItems.forEach((item) => {
-            if (item.image) {
-                item.image = Buffer.from(item.image).toString("base64");
-            }
-        });
+		// Convert BLOB data to Base64 for each cart item
+		cartItems.forEach((item) => {
+			if (item.image) {
+				item.image = Buffer.from(item.image).toString("base64");
+			}
+		});
 
-        // Calculate total price
-        const totalPrice = cartItems.reduce((total, item) => {
-            return total + (item.price * item.quantity);
-        }, 0);
+		// Calculate total price
+		const totalPrice = cartItems.reduce((total, item) => {
+			return total + item.price * item.quantity;
+		}, 0);
 
-        // Pass the update status and total price to the template
-        const updateStatus = req.query.update || '';
+		// Pass the update status and total price to the template
+		const updateStatus = req.query.update || "";
 
-        res.render('cart', {
-            title: 'Your Cart',
-            cartItems: cartItems,
-            updateStatus: updateStatus,
-            totalPrice: totalPrice 
-        });
-    });
+		res.render("cart", {
+			title: "Your Cart",
+			cartItems: cartItems,
+			updateStatus: updateStatus,
+			totalPrice: totalPrice,
+		});
+	});
 });
-
-
 
 // Route to handle updating the quantity of a product in the cart
-router.post('/update/:productId', authMiddleware, (req, res) => {
-    const productId = req.params.productId;
-    const userId = req.session.userdata.id;
-    const quantity = parseInt(req.body.quantity, 10);
+router.post("/update/:productId", authMiddleware, (req, res) => {
+	const productId = req.params.productId;
+	const userId = req.session.userdata.id;
+	const quantity = parseInt(req.body.quantity, 10);
 
-    // Ensure quantity is a positive number
-    if (isNaN(quantity) || quantity < 1) {
-        return res.status(400).send("Invalid quantity.");
-    }
+	// Ensure quantity is a positive number
+	if (isNaN(quantity) || quantity < 1) {
+		return res.status(400).send("Invalid quantity.");
+	}
 
-    const updateCartQuery = "UPDATE cart SET quantity = ? WHERE user_id = ? AND product_id = ?";
+	const updateCartQuery =
+		"UPDATE cart SET quantity = ? WHERE user_id = ? AND product_id = ?";
 
-    global.db.run(updateCartQuery, [quantity, userId, productId], function(err) {
-        if (err) {
-            console.error(err.message);
-            return res.status(500).send("Internal server error.");
-        }
-        // Redirect with a query parameter indicating success
-        res.redirect('/cart?update=success');
-    });
+	global.db.run(
+		updateCartQuery,
+		[quantity, userId, productId],
+		function (err) {
+			if (err) {
+				console.error(err.message);
+				return res.status(500).send("Internal server error.");
+			}
+			// Redirect with a query parameter indicating success
+			res.redirect("/cart?update=success");
+		}
+	);
 });
-
-
-
-
 
 module.exports = router;
--- routes/likes.js
+++ routes/likes.js
@@ -7,55 +7,62 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 
 // Route to handle displaying likes for the logged-in user
-router.get('/', authMiddleware, (req, res) => {
-    const userId = req.session.userdata && req.session.userdata.id;
+router.get("/", authMiddleware, (req, res) => {
+	const userId = req.session.userdata && req.session.userdata.id;
 
-    if (!userId) {
-        // If userId is undefined, send an error response
-        return res.status(401).json({ error: 'Sign in before viewing likes.' });
-    }
+	if (!userId) {
+		// If userId is undefined, send an error response
+		return res.status(401).json({ error: "Sign in before viewing likes." });
+	}
 
-    // Query to get likes for the user
-    global.db.all(`
+	// Query to get likes for the user
+	global.db.all(
+		`
         SELECT products.id, products.name, products.price, products.description, products.image, products.image_type 
         FROM likes 
         JOIN products ON likes.product_id = products.id 
         WHERE likes.user_id = ?
-    `, [userId], (err, rows) => {
-        if (err) {
-            console.error('Failed to retrieve likes:', err.message);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
+    `,
+		[userId],
+		(err, rows) => {
+			if (err) {
+				console.error("Failed to retrieve likes:", err.message);
+				res.status(500).send("Internal Server Error");
+				return;
+			}
 
-        // Convert BLOB data to Base64 for each liked product
-        rows.forEach(like => {
-            if (like.image) {
-                like.image = Buffer.from(like.image).toString('base64');
-            }
-        });
+			// Convert BLOB data to Base64 for each liked product
+			rows.forEach((like) => {
+				if (like.image) {
+					like.image = Buffer.from(like.image).toString("base64");
+				}
+			});
 
-        // Render likes page with the likes data
-        res.render('likes', { title: 'My Likes', likes: rows });
-    });
+			// Render likes page with the likes data
+			res.render("likes", { title: "My Likes", likes: rows });
+		}
+	);
 });
-
-
 
 // Route to handle removing a like
-router.post('/unlike/:productId', authMiddleware, (req, res) => {
-    const productId = req.params.productId;
-    const userId = req.session.userdata.id;
+router.post("/unlike/:productId", authMiddleware, (req, res) => {
+	const productId = req.params.productId;
+	const userId = req.session.userdata.id;
 
-    global.db.run('DELETE FROM likes WHERE user_id = ? AND product_id = ?', [userId, productId], function(err) {
-        if (err) {
-            console.error('Failed to remove like:', err.message);
-            return res.status(500).send('Internal Server Error');
-        }
+	global.db.run(
+		"DELETE FROM likes WHERE user_id = ? AND product_id = ?",
+		[userId, productId],
+		function (err) {
+			if (err) {
+				console.error("Failed to remove like:", err.message);
+				return res.status(500).send("Internal Server Error");
+			}
 
-        res.redirect('/likes?message=Product successfully removed from likes&type=success');
-    });
+			res.redirect(
+				"/likes?message=Product successfully removed from likes&type=success"
+			);
+		}
+	);
 });
-
 
 module.exports = router;
